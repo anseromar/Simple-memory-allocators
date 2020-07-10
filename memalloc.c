f779a6e274fb457b9c102812f9463312717f33a3
oanser <anseromar@outlook.fr>
2020-07-10 18:19:29 +0200

    malloc added

--- memalloc.c
+++ memalloc.c
@@ -1,3 +1,64 @@
 /*Only for the debug printf*/
 #include <stdio.h>
 
+#include <pthread.h>
+
+typedef char ALIGN[16];
+
+union header{
+
+    struct {
+        size_t size;
+        unsigned is_free;
+        union header *next;
+
+    } s;
+
+    ALIGN stub;
+};
+
+typedef union header header_t;
+
+header_t *head = NULL, header_t *tail = NULL;
+pthread_mutex_t global_malloc_lock;
+
+
+void *malloc(size_t size){
+    header_t *header;
+    size_t total_size;
+    void *block;
+
+    if(!size)
+        return NULL;
+    pthread_mutex_lock(&global_malloc_lock);
+    
+    header = get_free_block(size);
+    if (header){
+        header->s.is_free = 0;
+        pthread_mutex_unlock(&global_malloc_lock);
+        return (void*)(header + 1);
+    }
+    
+    total_size = sizeof(header_t) + size;
+    block = sbrk(total_size);
+    
+    if(block == (void*) -1){
+        pthread_mutex_unlock(&global_malloc_lock);
+        return NULL;
+    }
+
+    header = (header_t *)block;
+    header->s.size = size;
+    header->s.is_free = 0;
+    header->s.next = NULL;
+
+    if(!head){
+        head = header;
+    }
+    if(tail)
+        tail->s.next = header;
+    tail = header;
+
+    pthread_mutex_unlock(&global_malloc_lock);
+    return (void*)(header + 1);
+}
