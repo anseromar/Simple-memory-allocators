17b11e3c4b243086ad35a688da6f64e33492d389
anseromar <anseromar@outlook.fr>
2020-07-16 16:43:31 +0200

    free calloc and realloc

--- memalloc.c
+++ memalloc.c
@@ -1,6 +1,8 @@
 /*Only for the debug printf*/
 #include <stdio.h>
 
+#include <string.h>
+#include <unistd.h>
 #include <pthread.h>
 
 typedef char ALIGN[16];
@@ -31,17 +33,17 @@ void *malloc(size_t size){
     if(!size)
         return NULL;
     pthread_mutex_lock(&global_malloc_lock);
-    
+
     header = get_free_block(size);
     if (header){
         header->s.is_free = 0;
         pthread_mutex_unlock(&global_malloc_lock);
         return (void*)(header + 1);
     }
-    
+
     total_size = sizeof(header_t) + size;
     block = sbrk(total_size);
-    
+
     if(block == (void*) -1){
         pthread_mutex_unlock(&global_malloc_lock);
         return NULL;
@@ -61,4 +63,70 @@ void *malloc(size_t size){
 
     pthread_mutex_unlock(&global_malloc_lock);
     return (void*)(header + 1);
+}
+
+void free(void *block)
+{
+    header_t *header, *tmp;
+    void *programbreak;
+
+    if(!block)
+        return;
+    pthread_mutex_lock(&global_malloc_lock);
+    header = (header_t*)block - 1;
+
+    programbreak = sbrk(0);
+    if((char*)block + header->s.size == programbreak){
+        if(head == tail){
+            head = tail = NULL;
+        }else{
+            tmp = head;
+            while(tmp){
+                if(tmp->s.next == tail){
+                    tmp->s.next = NULL;
+                    tail = tmp;
+                }
+                tmp = tmp->s.next;
+            }
+        }
+        sbrk(0 - sizeof(header_t) - header->s.size);
+        pthread_mutex_unlock(&global_malloc_lock);
+		return;
+    }
+    header->s.is_free = 1;
+    pthread_mutex_unlock(&global_malloc_lock);
+}
+
+void *calloc(size_t num, size_t nsize)
+{
+    if(!num || !nsize)
+        return NULL;
+    size = num * nsize;
+    /* check mul overflow*/
+    if(nsize != size / num)
+        return NULL;
+    block = malloc(size);
+    if(!block)
+        return NULL;
+    memset(block,0,size);
+    return block;
+}
+
+void *realloc(void *block, size_t size)
+{
+    header_t *header;
+    void *ret;
+
+    if(!block || !size)
+        return malloc(size);
+    header = (header_t*)block -1;
+    if (header->s.size >= size)
+        return block;
+    ret = malloc(size);
+    if (ret) {
+
+        memcpy(ret, block, header->s.size);
+        free(block);
+    }
+    return ret;
 }
